5941e8e4f8b6ce8571147fcf6b495fd2b10db8ad
Ziortza Astigarraga <ziortza.a@opendeusto.es>
2025-10-09 08:43:23 +0200

    WeightedAWithOtherGraphs

--- Weighted A.py
+++ Weighted A.py
@@ -1,9 +1,14 @@
-
+import datagraph1
+import datagraph2
+import datagraph3
 import Grid
 import queue
 import heapq
 
-problema=Grid.grid
+#problema=datagraph1.graph
+#problema=datagraph2.graph
+problema=datagraph3.graph
+
 
 class Node:
     def __init__(self,state,parent=None,g=0,h=0):
@@ -17,19 +22,27 @@ class Node:
         return self.f < other.f
 
 class Problema:
-    def __init__(self,grid):
-        self.grid=grid
-        self.rows=len(grid)
-        self.cols=len(grid[0])
-        self.initial_state=self.find_symbol('S')
-        self.final_state=self.find_symbol('G')
+    # def __init__(self,grid):
+    #     self.grid=grid
+    #     self.rows=len(grid)
+    #     self.cols=len(grid[0])
+    #     self.initial_state=self.find_symbol('S')
+    #     self.final_state=self.find_symbol('G')
+
+    #def __init__(self, graph,h_values,initial_state='S', final_state='G'): (Adecuar al inicio y al final de cada graph)
+    def __init__(self, graph,h_values,initial_state='S', final_state='Z'):
+        self.graph = graph
+        self.h_values = h_values
+        self.initial_state = initial_state
+        self.final_state = final_state
+
     
-    def find_symbol(self,symbol):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if self.grid[i][j] == symbol:
-                    return(i,j)
-        return None
+    # def find_symbol(self,symbol):
+    #     for i in range(self.rows):
+    #         for j in range(self.cols):
+    #             if self.grid[i][j] == symbol:
+    #                 return(i,j)
+    #     return None
     
 
     def is_goal(self,state):
@@ -37,21 +50,24 @@ class Problema:
     
 
     def heuristic(self,state):
-        gx,gy = self.final_state
-        x,y = state
-        return abs(gx-x)+ abs(gy-y)
+        # gx,gy = self.final_state
+        # x,y = state
+        # return abs(gx-x)+ abs(gy-y)
+        return self.h_values[state]
+    
     
     def get_nextNodes(self,state):
-        nextNodes= []
-        legalMovements=[(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for dx, dy in legalMovements:
-            x,y=state
-            nx,ny = x+dx, y+dy
+        # nextNodes= []
+        # legalMovements=[(-1, 0), (1, 0), (0, -1), (0, 1)]
+        # for dx, dy in legalMovements:
+        #     x,y=state
+        #     nx,ny = x+dx, y+dy
 
-            if 0<= nx < self.rows and 0<=ny < self.cols:
-                if self.grid[nx][ny]!='X':
-                    nextNodes.append(((nx,ny),1))
-        return nextNodes
+        #     if 0<= nx < self.rows and 0<=ny < self.cols:
+        #         if self.grid[nx][ny]!='X':
+        #             nextNodes.append(((nx,ny),1))
+        # return nextNodes
+        return list(self.graph[state].items())
     
 def final_path(node):
     path=[]
@@ -94,13 +110,25 @@ def AStarSearch(problema,w1=1,w2=1.5):
 
 #iniciar
 if __name__=="__main__":
-    problema=Problema(Grid.grid)
-    path = AStarSearch(problema)
+    # problema=Problema(Grid.grid)
+    # path = AStarSearch(problema)
 
+    # if path:
+    #     print("Ruta:")
+    #     print (path)
+    #     print(f"Path length: {len(path) - 1} moves")
+
+    # else:
+    #     print("no se ha podido encontrar ninguna solucion")
+
+
+    #problema=Problema(datagraph1.graph,datagraph1.h_values)
+    #problema=Problema(datagraph2.graph,datagraph2.h_values)
+    problema=Problema(datagraph3.graph,datagraph3.h_values)
+    path = AStarSearch(problema)
     if path:
         print("Ruta:")
         print (path)
         print(f"Path length: {len(path) - 1} moves")
-
     else:
         print("no se ha podido encontrar ninguna solucion")
